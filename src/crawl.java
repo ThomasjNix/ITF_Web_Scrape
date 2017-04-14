4ae7ac3e80b83f66bc53b0b1a478a6009c825617
Thomas Nix <tnix2@uncc.edu>
2017-04-14 12:12:01 -0400

    Completed. Now processes all files in the Player_DOMs directory and exports a file of all players and their highest round won in each grade in a CSV format (? for missing values, any corrupted data not included).

--- src/crawl.java
+++ src/crawl.java
@@ -1,83 +1,177 @@
+/*
+ * Written by Thomas Nix
+ * For the purposes of the T1 ITF Tennis project
+ * In ITCS-3162 Section 001 at UNC Charlotte
+ * 
+ * Project partners: Jesus Garcia and Seth Morris
+ */
+
 import java.io.*;
 import java.util.*;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 public class crawl {
 
-	public static void main(String args[]) throws IOException{
-		/*
-		 * Instantiate document object instance and loop through folder containing HTML pages
-		 */
+	/*
+	 * Main driver class to load in data, organize it, distinguish it, and export it to a file.
+	 */
+	public static void main(String args[]) throws IOException {
+		//Creating file writer for later 
+		//and writing the header information
+		File stats_file = new File("PlayerStats.txt");
+		FileWriter write_file = new FileWriter(stats_file);
+		write_file.write("player_name,player_yob,grade_1,grade_a,grade_2,grade_b,grade_3,grade_c,grade_4,grade_5\n");
 		
+		// Reading in all files from the directory
 		Document playerProfilePage;
 		final File readDirectory = new File("Player_DOMs");
 		Player[] playerArray = new Player[readDirectory.listFiles().length];
 		int playerCounter = 0;
-		for (File single_file : readDirectory.listFiles()){
+		
+		// Looping through each file and gathering player data
+		for (File single_file : readDirectory.listFiles()) {
 			playerProfilePage = Jsoup.parse(single_file, "UTF-8");
-			
-			/*
-			 * Getting the player information
-			 */
-			String playerName = playerProfilePage.select("#PlayerDiv > div.pn-in.clfx > div > div.playerDetails.fl > ul > li:nth-child(1) > strong").text();
+			String playerName = playerProfilePage
+					.select("#PlayerDiv > div.pn-in.clfx > div > div.playerDetails.fl > ul > li:nth-child(1) > strong")
+					.text();
 			String fullDOB = playerProfilePage.select("#liDOB > strong").text();
 			String yearDOB = fullDOB.substring(fullDOB.length() - 4, fullDOB.length());
 			int playerYearOfBirth = Integer.parseInt(yearDOB);
-			if (playerYearOfBirth == 1999 || playerYearOfBirth == 2000 || playerYearOfBirth == 2001){
-				System.out.println("Player name: " + playerName + "\t Player birth year: " + playerYearOfBirth + "\n====================");
-				/*
-				 * Here I gather all the information from the players table data.
-				 * I break it down into the specific tables that are for singles only,
-				 * then I parse each row that does not contain missing values 
-				 * and break the table data for the row into the important pieces
-				 * 
-				 * To filter out what contains missing values, I saw every missing value table row
-				 * contained the content "BYE" in the td.td-120 table data, so I excluded all rows that contained
-				 * the phrase "BYE".
-				 */
-	
-				int numGamesTotal = playerProfilePage.select("li.pn-Gradient").size();
-				Elements grade = null;
-				String highest_level = "";
-				String highest_level_win_loss = "";
-				HashMap<Integer, String> playerData = new HashMap<Integer, String>();
-				for (Element currentElm : playerProfilePage.select("li.pn-Gradient")){
-					for (Element matchData : currentElm.select("ul:nth-of-type(2)")){
-						if (matchData.select("li").text().charAt(0) == 'S'){
-							grade=matchData.select("li:nth-of-type(2)");
-							
-							for (Element tableRow : currentElm.select("ul:nth-of-type(2) + table tbody tr:nth-of-type(n+3)")){
+			if (playerYearOfBirth == 1999 || playerYearOfBirth == 2000 || playerYearOfBirth == 2001) {
+				
+				// Creating the new player
+				playerArray[playerCounter] = new Player(playerName, playerYearOfBirth);
+				String grade = null;
+				HashMap<String, String[]> playerScoreCard = new HashMap<String, String[]>();
+				
+				// Looping through each match and gathering data on it
+				for (Element currentElm : playerProfilePage.select("li.pn-Gradient")) {
+					for (Element matchData : currentElm.select("ul:nth-of-type(2)")) {
+						if (matchData.select("li").text().charAt(0) == 'S') {
+							grade = matchData.select("li:nth-of-type(2)").text();
+
+							// Normalizing grade values
+							NormalizeGrade ng = new NormalizeGrade(grade);
+							grade = ng.newGrade;
+
+							// Getting specific rounds and scores
+							for (Element tableRow : currentElm
+									.select("ul:nth-of-type(2) + table tbody tr:nth-of-type(n+3)")) {
 								String round = null;
 								String round_score = null;
-								
-								if (!(tableRow.select("td").text().replaceAll(" ", "").contains("BYE")) &&
-										!((tableRow.select("td.td-20").text().length() == 0) || (tableRow.select("td.td-30").text().length() == 0))){
+
+								// Separating each round into identifiable parts
+								if (!(tableRow.select("td").text().replaceAll(" ", "").contains("BYE"))
+										&& !((tableRow.select("td.td-20").text().length() == 0)
+												|| (tableRow.select("td.td-30").text().length() == 0))) {
 									round = tableRow.select("td.td-20").text();
 									round_score = tableRow.select("td.td-30").text();
-									System.out.println("Grade: " + grade.text() + "\tRound:" + round + "\tRound Score: " + round_score);
-									
+
+
+									// Updating the player scorecard
 									/*
-									 * Implement highest level / win
+									 * For each grade
+									 *  --update the highest round played ONLY if: 
+									 * ===1: The round is the same or higher AND 
+									 * ===2: The higher round is a WIN
+									 * -----
+									 * (ex1: old: Grade 3, round 64, W new: Grade 3, round 32, W) -> take new
+									 * (ex2: old: Grade 3, round 64, L new: Grade 3, round 64, W) -> take new
+									 * (ex3: old: Grade 3, round 32, W new: Grade 3, round 32, L) -> leave old
+									 * (ex4: old: Grade 3, round 64, L new: Grade 3, round 32, L) -> leave old
 									 */
+
+									double temporaryRoundScore = 99; // for use with non-numeric rounds
+									double temporaryRoundScore2 = 99; // for use with non-numeric rounds
+										
+									if (round_score.contains("W") && (!round.contains("Q1") && !round.contains("Q2")
+											&& !round.contains("Q3"))) {
+										if (playerScoreCard.get(grade) == null) {
+											playerScoreCard.put(grade, new String[] { round, round_score });
+										} else {
+											try {
+												int previousRoundScore = Integer
+														.parseInt(playerScoreCard.get(grade)[0]);
+												try {
+
+													int currentRoundScore = Integer.parseInt(round);
+													if (currentRoundScore <= previousRoundScore) {
+														playerScoreCard.put(grade, new String[] { round, round_score });
+													}
+												} catch (Exception e) {
+
+													if (round == "QF") {
+														temporaryRoundScore = 3;
+													} else if (round == "SF") {
+														temporaryRoundScore = 2;
+													} else if (round == "FR") {
+														temporaryRoundScore = 1;
+													} else {
+														// Throw away invalid
+														// data that doesn't
+														// match
+														// 64/32/16/QF/SF/FR
+														temporaryRoundScore = 99;
+													}
+
+													if (temporaryRoundScore <= previousRoundScore) {
+														playerScoreCard.put(grade, new String[] { round, round_score });
+													}
+												}
+											} catch (Exception e) {
+												if (playerScoreCard.get(grade)[0] == "QF") {
+													temporaryRoundScore = 3;
+												} else if (playerScoreCard.get(grade)[0] == "SF") {
+													temporaryRoundScore = 2;
+												} else if (playerScoreCard.get(grade)[0] == "FR") {
+													temporaryRoundScore = 1;
+												} else {
+													// Throw away invalid data
+													// that doesn't match
+													// 64/32/16/QF/SF/FR
+													temporaryRoundScore = 4;
+												}
+												
+												//No try/catch block necessary here since we know that the previous round is non-numeric 
+												//and we only care about the current round if it is also non-numeric
+												if (round == "QF") {
+													temporaryRoundScore2 = 3;
+												} else if (round == "SF") {
+													temporaryRoundScore2 = 2;
+												} else if (round == "FR") {
+													temporaryRoundScore2 = 1;
+												} else {
+													// Throw away invalid
+													// data that doesn't
+													// match
+													// 64/32/16/QF/SF/FR
+													temporaryRoundScore2 = 5;
+												}
+												if (temporaryRoundScore2 <= temporaryRoundScore) {
+													playerScoreCard.put(grade, new String[] { round, round_score });
+												}	
+											}
+										}
+
+									}
+									playerArray[playerCounter].updateScorecard(playerScoreCard);
 								}
-								
-							}	
+							}
 						}
 					}
 				}
-				System.out.println("====================\n\n");
 			}
+			
+			//Sets up the printable string and adds a new line so the file writer can write by line
+			String line_to_write = playerArray[playerCounter].createPrintableInstance().substring(0, playerArray[playerCounter].createPrintableInstance().length()-1);
+			line_to_write += "\n";
+			write_file.write(line_to_write);
+			playerCounter++;
 		}
-		
+		//Closing the file writer once all files have been processed.
+		write_file.close();
 
-		
 	}
 }
-
-
-
-
